9fe648ccc53120698a48bdc857e3104104813c09
Astae <mathiscrochet@gmail.com>
2022-03-23 10:52:17 +0100

    Création du projet

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv)
+{
+    cout << "Hello, World!" << endl;
     return 0;
 }
