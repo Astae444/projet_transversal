db98da0ef3dd809b9b5fffa4c71e03df8b6eb01c
Xaruda <noe.bonnet6@gmail.com>
2022-04-06 13:59:01 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
+
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     cout << "Hello, World!" << endl;
     return 0;
 }
 
-class Argent{
+class Tourelle {
+protected:
+public:
+    int vieMax;
+    int vie;
+    int attaque;
+
+};
+
+class Monstre {
+protected:
+public:
+    int vieMax;
+    int vie;
+    int attaque;
+    int vitesse;
+
+};
+
+class Argent {
     int vie = 50;
 
 };
-class Soin{
+
+class Soin {
     int vie = 50;
 
 };
-class Mittraillette{
+
+class Mittraillette {
     int vie = 100;
     int degats = 5;
     int range = 5;
 
 };
-class Sniper{
+
+class Sniper {
     int vie = 100;
     int degats = 3;
     int range = 8;
 
 };
-class Shotgun{
+
+class Shotgun {
     int vie = 100;
     int degats = 10;
     int range = 3;
 
 };
-class Minion{
+
+class Minion {
     int vie = 25;
     int degats = 10;
     int vitesse = 1;
 
 };
-class Boss{
+
+class Boss {
     int vie = 50;
     int degats = 20;
     int vitesse = 2;
