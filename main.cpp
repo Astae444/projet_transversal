fee5b00cd6e12b0141896691b952f93bb42e8912
Xaruda <noe.bonnet6@gmail.com>
2022-04-06 11:05:19 +0200

    .

--- main.cpp
+++ main.cpp
@@ -6,3 +6,42 @@ int main(int argc, char **argv)
     cout << "Hello, World!" << endl;
     return 0;
 }
+
+class Argent{
+    int vie = 50;
+
+};
+class Soin{
+    int vie = 50;
+
+};
+class Mittraillette{
+    int vie = 100;
+    int degats = 5;
+    int range = 5;
+
+};
+class Sniper{
+    int vie = 100;
+    int degats = 3;
+    int range = 8;
+
+};
+class Shotgun{
+    int vie = 100;
+    int degats = 10;
+    int range = 3;
+
+};
+class Minion{
+    int vie = 25;
+    int degats = 10;
+    int vitesse = 1;
+
+};
+class Boss{
+    int vie = 50;
+    int degats = 20;
+    int vitesse = 2;
+
+};
